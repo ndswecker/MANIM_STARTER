84e7d6f2a712b0353a38600bed9252326ba60477
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-04 15:53:43 -0800

    Slider on top of base
    Not yet correctly placed

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -3,11 +3,13 @@ from manim import *
 class BaseBlock(Scene):
     def construct(self):
         base = Polygon([0,0,0], [0,5,0], [5,0,0])
+        slider = Square(side_length=1).rotate(PI/4).to_edge(LEFT, buff=1).to_edge(DOWN, buff=5)
 
+        # Place the base on in the bottom left corner
         base.to_edge(LEFT, buff=1)
         base.to_edge(DOWN, buff=1)
 
-        self.play(Create(base))
+        self.play(Create(base), Create(slider))
 
         self.wait(2)
 
