1c001f70ab2ef9f4770f45ef9db0f02edeaa8122
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-04 19:13:34 -0800

    Stuck at placing slider in correct starting position

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -3,9 +3,21 @@ from manim import *
 
 class BaseBlock(Scene):
     def construct(self):
-        ps = math.sqrt(1/2)
-        base = Polygon([0,0,0], [0,5,0], [5,0,0])
-        slider = Square(side_length=1).rotate(PI/4)
+
+        # Ramp paramaterized
+        RAMP_HEIGHT = 4
+        RAMP_BASE = 6
+        RAMP_HYP = math.sqrt(RAMP_HEIGHT**2 + RAMP_BASE**2)
+        RAMP_ANGLES = [(PI/2), (math.acos(RAMP_BASE/RAMP_HYP)), (math.acos(RAMP_HEIGHT/RAMP_HYP))]
+
+        # Slider paramaterized
+        SLIDER = 1
+
+        RAMP_COORD = [[0,0,0], [RAMP_BASE,0,0], [0,RAMP_HEIGHT,0]]
+
+        ps = math.sqrt((SLIDER**2)/2)
+        base = Polygon(RAMP_COORD[0], RAMP_COORD[1], RAMP_COORD[2])
+        slider = Square(side_length=SLIDER)
 
         # Place the base on in the bottom left corner
         base.to_edge(LEFT, buff=1)
@@ -13,13 +25,14 @@ class BaseBlock(Scene):
 
 
         # Place the slider to the far left
-        slider.to_edge(LEFT, buff=1)
-        slider.to_edge(DOWN,buff=6-ps)        
+        slider.to_edge(LEFT, buff=(1+(SLIDER/(math.sqrt(SLIDER)))))
+        slider.to_edge(DOWN,buff=(RAMP_HEIGHT+1-(SLIDER/(math.sqrt(SLIDER)))))        
 
         # Place the 
 
         self.play(Create(slider), Create(base))
-
+        self.wait(1)
+        self.play(slider.animate.rotate(RAMP_ANGLES[2]))
         self.wait(2)
 
         
