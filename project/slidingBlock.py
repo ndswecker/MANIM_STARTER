e8486796b2cdf6f85a8b03373e48c8a34cab11f2
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-04 17:25:37 -0800

    Slider adjusted to fixed sized Base

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -1,15 +1,24 @@
+import math
 from manim import *
 
 class BaseBlock(Scene):
     def construct(self):
+        ps = math.sqrt(1/2)
         base = Polygon([0,0,0], [0,5,0], [5,0,0])
-        slider = Square(side_length=1).rotate(PI/4).to_edge(LEFT, buff=1).to_edge(DOWN, buff=5)
+        slider = Square(side_length=1).rotate(PI/4)
 
         # Place the base on in the bottom left corner
         base.to_edge(LEFT, buff=1)
         base.to_edge(DOWN, buff=1)
 
-        self.play(Create(base), Create(slider))
+
+        # Place the slider to the far left
+        slider.to_edge(LEFT, buff=1)
+        slider.to_edge(DOWN,buff=6-ps)        
+
+        # Place the 
+
+        self.play(Create(slider), Create(base))
 
         self.wait(2)
 
