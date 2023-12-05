6a21641080f6f1490de30b9719c0b963f9b48dcb
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-05 14:34:05 -0800

    Simplify to_edge to one line

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -5,29 +5,26 @@ class BaseBlock(Scene):
     def construct(self):
 
         # Ramp paramaterized
-        RAMP_HEIGHT = 2
-        RAMP_BASE = 9
+        RAMP_HEIGHT = 5
+        RAMP_BASE = 7
         RAMP_HYP = math.sqrt(RAMP_HEIGHT**2 + RAMP_BASE**2)
         RAMP_ANGLES = [(PI/2), (math.acos(RAMP_BASE/RAMP_HYP)), (math.acos(RAMP_HEIGHT/RAMP_HYP))]
         
 
         # Slider paramaterized
-        SLIDER = 1
+        SLIDER = 2.5
 
         RAMP_COORD = [[0,0,0], [RAMP_BASE,0,0], [0,RAMP_HEIGHT,0]]
 
-        ps = math.sqrt((SLIDER**2)/2)
         base = Polygon(RAMP_COORD[0], RAMP_COORD[1], RAMP_COORD[2])
         slider = Square(side_length=SLIDER)
 
         # Place the base on in the bottom left corner
-        base.to_edge(LEFT, buff=1)
-        base.to_edge(DOWN, buff=1)
-
+        base.to_edge(DOWN, buff=1).to_edge(LEFT, buff=1)
 
         # Place the slider to the far left
         slider.to_edge(LEFT, buff=1)
-        slider.to_edge(DOWN,buff=RAMP_HEIGHT+1)        
+        slider.to_edge(DOWN, buff=RAMP_HEIGHT + 1)        
 
         # Place the 
 
