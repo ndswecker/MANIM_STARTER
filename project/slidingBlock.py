5b56737b7f5a410de3f1de398469f8a5b5da2a2a
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-05 16:49:22 -0800

    Placement of X vector on correct location
    Not correctly sequenced in time yet

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -12,11 +12,16 @@ class BaseBlock(Scene):
 
         # Slider paramaterized
         SLIDER = 2.5
-
+        SLIDER_REF = [-6,-3 + RAMP_HEIGHT,0]
+        OFFSET = (SLIDER * math.sin(RAMP_ANGLES[1]))/2
         RAMP_COORD = [[0,0,0], [RAMP_BASE,0,0], [0,RAMP_HEIGHT,0]]
 
         base = Polygon(*RAMP_COORD)
         slider = Square(side_length=SLIDER)
+
+        # Direction Vectors
+        vctX = Vector([1,0]).move_to([-6 + SLIDER/math.cos(RAMP_ANGLES[1]) + OFFSET, -3 + RAMP_HEIGHT, 0])
+        vctY = Vector([0,-1])
 
         # Place the base on in the bottom left corner
         base.to_edge(DOWN, buff=1).to_edge(LEFT, buff=1)
@@ -25,15 +30,15 @@ class BaseBlock(Scene):
         slider.to_edge(LEFT, buff=1).to_edge(DOWN, buff=RAMP_HEIGHT + 1)        
 
         # Place the Ramp and the slider on the scene
-        self.play(Create(slider), Create(base))
+        self.play(Create(slider), Create(base), Create(vctX))
         self.wait(1)
-        
+
         # Rotate the slider into the correct angle
         self.play(
             Rotate(
                 slider,
                 angle=-RAMP_ANGLES[1],
-                about_point=[-6,-3+RAMP_HEIGHT,0]
+                about_point=[*SLIDER_REF]
             )
         )
         self.wait(1)
