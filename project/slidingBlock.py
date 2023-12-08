8f5459ea34251ba1f7c595e600f89a4894a977d7
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-08 15:45:22 -0800

    Ramp dim displaying. Initial theta rendering
    need to dynamically set theta location at visually pleasing location.

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -9,27 +9,34 @@ class BaseBlock(Scene):
 
 
         # Ramp paramaterized
-        RAMP_HEIGHT = 5
-        RAMP_BASE = 10
+        RAMP_HEIGHT = 2
+        RAMP_BASE = 7
         RAMP_HYP = math.sqrt(RAMP_HEIGHT**2 + RAMP_BASE**2)
         # Angles Start at 90 and go ccw
         RAMP_ANGLES = [(PI/2), (math.acos(RAMP_BASE/RAMP_HYP)), (math.acos(RAMP_HEIGHT/RAMP_HYP))]
 
         # Slider paramaterized
-        SLIDER = 2
+        SLIDER = 1.5
         SLIDER_REF = [-6,-3 + RAMP_HEIGHT,0]
         sliderMass = SLIDER**2 * DENSITY
         slider = Square(side_length=SLIDER, fill_opacity=0.5)
         sliderFG = ACC_G * sliderMass * math.sin(RAMP_ANGLES[1])
 
-        # Base ramp consturcted from height and base length with 90 degree
+        # Ramp consturcted from height and base length with 90 degree
         # angle at corner: [-6, -3, 0]
         RAMP_COORD = [[-6, -3, 0],
                       [-6 + RAMP_BASE, -3,0],
                       [-6, -3 + RAMP_HEIGHT, 0]]
-        base = Polygon(*RAMP_COORD, fill_opacity=0.5)
+        ramp = Polygon(*RAMP_COORD, fill_opacity=0.5)
 
-        
+        # Background Plane
+        planeBG = NumberPlane(
+            background_line_style={
+                "stroke_width": 2,
+                "stroke_opacity": 0.3
+            }
+        )
+        self.add(planeBG)  
         
         # Direction Vectors
         lengthVector = SLIDER
@@ -39,18 +46,26 @@ class BaseBlock(Scene):
         vctN = Vector([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1]))])
         vctD2 = vctD.copy()
 
-        # Place the slider to the far left ontop of ramp
-        slider.move_to(SLIDER_REF, aligned_edge=DL)    
+        # TEXT
+        forceGravityText = MathTex(r"F_g&", font_size=60).move_to((3,3,0), aligned_edge=UR)
+        theta = MathTex(r'\theta').move_to(ramp.get_critical_point(DR), aligned_edge=(RIGHT + DOWN)).shift([-RAMP_BASE/8, 0.2, 0])
+        # DIMENSIONS
+        rampHeightText = Integer(number=RAMP_HEIGHT).move_to(ramp.get_critical_point(LEFT), aligned_edge=RIGHT).shift([-0.2,0,0])
+        rampBaseText = Integer(number=RAMP_BASE).move_to(ramp.get_critical_point(DOWN), aligned_edge=UP).shift([0,-0.2,0])
+        # ANGLES
+        line1 = Line(start=RAMP_COORD[1], end=RAMP_COORD[2])
+        line2 = Line(start=RAMP_COORD[1], end=RAMP_COORD[0])
+        thetaArc = Angle(line1, line2, radius=(RAMP_BASE/4))
+        self.add(forceGravityText, rampHeightText, rampBaseText, thetaArc, theta)
 
-        # Background Plane
-        planeBG = NumberPlane()
-        self.add(planeBG)    
+        # Place the slider to the far left ontop of ramp
+        slider.move_to(SLIDER_REF, aligned_edge=DL)      
 
         # Place the Ramp and the slider on the scene
-        topCorner = Cross(scale_factor=0.1).move_to(base.get_critical_point(UL))
+        topCorner = Cross(scale_factor=0.1).move_to(ramp.get_critical_point(UL))
 
         self.play(Create(slider),
-                  Create(base),
+                  Create(ramp),
                   Create(topCorner))
         self.wait(1)
 
