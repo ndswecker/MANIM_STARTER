24c8e9bbe2e5f956947be53b7399ade09994540a
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-09 12:59:44 -0800

    Force of Gravity and Displacement force Formulae

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -5,25 +5,22 @@ class BaseBlock(Scene):
     def construct(self):
 
         ACC_G = 9.8
-        DENSITY = 0.3
+        DENSITY = 0.1
 
         # Ramp paramaterized
-        RAMP_HEIGHT = 4
-        RAMP_BASE = 10
+        RAMP_HEIGHT = 5
+        RAMP_BASE = 7
         RAMP_HYP = math.sqrt(RAMP_HEIGHT**2 + RAMP_BASE**2)
         # Angles Start at 90 and go ccw
         RAMP_ANGLES = [(PI/2), (math.acos(RAMP_BASE/RAMP_HYP)), (math.acos(RAMP_HEIGHT/RAMP_HYP))]
 
         # Slider paramaterized
-        SLIDER = 1
+        SLIDER = 1.5
         SLIDER_REF = [-6,-3 + RAMP_HEIGHT,0]
         sliderMass = SLIDER**2 * DENSITY
         slider = Square(side_length=SLIDER, fill_opacity=0.5)
         sliderFGy = ACC_G * sliderMass
-        sliderFGparallel = ACC_G * sliderMass * math.sin(RAMP_ANGLES[1])
-        sliderFGVar = Variable(sliderFGy, MathTex(r'F_g'), num_decimal_places=1).move_to([3,3,0])
-        sliderFGVar.label.set_color(WHITE)
-        sliderFGVar.value.set_color(RED)
+        sliderDispMag = -(ACC_G * sliderMass) * math.sin(RAMP_ANGLES[1])
 
         # Ramp consturcted from height and base length with 90 degree
         # angle at corner: [-6, -3, 0]
@@ -44,17 +41,24 @@ class BaseBlock(Scene):
         # Direction Vectors
         lengthVector = SLIDER
         vctX = Vector([lengthVector,0])
+        # Force of Gravity Vector, in negative Y direction
         vctY = Vector([0,-(ACC_G * sliderMass)])
-        
+        # Displacement Vector, and its copy to use for reference
         vctD = Vector([0, -(ACC_G * sliderMass) * math.sin(RAMP_ANGLES[1]), 0]).rotate(about_point=(slider.get_critical_point((0,0,0))), angle=(RAMP_ANGLES[2]))
-        #vctD = Vector([math.cos(RAMP_ANGLES[1]) * sliderFGparallel, -math.sin(RAMP_ANGLES[1]) * sliderFGparallel])
-        #vctN = Vector([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1]))])
-        vctN = Vector([0, -(ACC_G * sliderMass) * math.cos(RAMP_ANGLES[1]), 0]).rotate(about_point=(slider.get_critical_point((0,0,0))), angle=-RAMP_ANGLES[1])
         vctD2 = vctD.copy()
+        # Normal vector of slider on ramp
+        vctN = Vector([0, -(ACC_G * sliderMass) * math.cos(RAMP_ANGLES[1]), 0]).rotate(about_point=(slider.get_critical_point((0,0,0))), angle=-RAMP_ANGLES[1])
 
-        # TEXT
+        # TEXT 
         theta = MathTex(r'\theta').move_to([RAMP_COORD[1][0] - RAMP_BASE/4 - 0.2 , RAMP_COORD[1][1] + 0.2,0])
-        # DIMENSIONS
+        sliderFGVar = Variable(sliderFGy, MathTex(r'F_g (mg)'), num_decimal_places=1).move_to([4, 3 ,0])
+        sliderFGVar.label.set_color(WHITE)
+        sliderFGVar.value.set_color(RED)
+        displacementVar = Variable(-sliderDispMag, MathTex(r'F_D (mg*sin\theta)'), num_decimal_places=1).next_to(sliderFGVar, DOWN, aligned_edge=RIGHT)
+        displacementVar.label.set_color(WHITE)
+        displacementVar.value.set_color(RED)
+
+        # DIMENSIONS of Ramp dispayed
         rampHeightText = Integer(number=RAMP_HEIGHT).move_to(ramp.get_critical_point(LEFT), aligned_edge=RIGHT).shift([-0.2,0,0])
         rampBaseText = Integer(number=RAMP_BASE).move_to(ramp.get_critical_point(DOWN), aligned_edge=UP).shift([0,-0.2,0])
         
@@ -80,7 +84,8 @@ class BaseBlock(Scene):
                 about_point=[*SLIDER_REF]
             ),
             Create(thetaArc),
-            Write(sliderFGVar)
+            Write(sliderFGVar),
+            Write(displacementVar)
         )
         self.add(theta)
         self.wait(1)
