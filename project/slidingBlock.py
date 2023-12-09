e4c41abaafe1359df364abf44d8b239dd6ae35ad
Nathanael Swecker <nathanael.swecker@gmail.com>
2023-12-09 11:38:01 -0800

    Force of gravity parallel to movement vector placement
    use of get_vector() to shift displacement vector into position

--- project/slidingBlock.py
+++ project/slidingBlock.py
@@ -5,22 +5,23 @@ class BaseBlock(Scene):
     def construct(self):
 
         ACC_G = 9.8
-        DENSITY = 0.1
+        DENSITY = 0.3
 
         # Ramp paramaterized
-        RAMP_HEIGHT = 3
-        RAMP_BASE = 8
+        RAMP_HEIGHT = 4
+        RAMP_BASE = 10
         RAMP_HYP = math.sqrt(RAMP_HEIGHT**2 + RAMP_BASE**2)
         # Angles Start at 90 and go ccw
         RAMP_ANGLES = [(PI/2), (math.acos(RAMP_BASE/RAMP_HYP)), (math.acos(RAMP_HEIGHT/RAMP_HYP))]
 
         # Slider paramaterized
-        SLIDER = 2
+        SLIDER = 1
         SLIDER_REF = [-6,-3 + RAMP_HEIGHT,0]
         sliderMass = SLIDER**2 * DENSITY
         slider = Square(side_length=SLIDER, fill_opacity=0.5)
-        sliderFG = ACC_G * sliderMass * math.sin(RAMP_ANGLES[1])
-        sliderFGVar = Variable(sliderFG, MathTex(r'F_g'), num_decimal_places=1).move_to([3,3,0])
+        sliderFGy = ACC_G * sliderMass
+        sliderFGparallel = ACC_G * sliderMass * math.sin(RAMP_ANGLES[1])
+        sliderFGVar = Variable(sliderFGy, MathTex(r'F_g'), num_decimal_places=1).move_to([3,3,0])
         sliderFGVar.label.set_color(WHITE)
         sliderFGVar.value.set_color(RED)
 
@@ -44,13 +45,14 @@ class BaseBlock(Scene):
         lengthVector = SLIDER
         vctX = Vector([lengthVector,0])
         vctY = Vector([0,-(ACC_G * sliderMass)])
-        vctD = Vector([math.cos(RAMP_ANGLES[1]) * sliderFG, -math.sin(RAMP_ANGLES[1]) * sliderFG])
-        vctN = Vector([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1]))])
+        
+        vctD = Vector([0, -(ACC_G * sliderMass) * math.sin(RAMP_ANGLES[1]), 0]).rotate(about_point=(slider.get_critical_point((0,0,0))), angle=(RAMP_ANGLES[2]))
+        #vctD = Vector([math.cos(RAMP_ANGLES[1]) * sliderFGparallel, -math.sin(RAMP_ANGLES[1]) * sliderFGparallel])
+        #vctN = Vector([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1]))])
+        vctN = Vector([0, -(ACC_G * sliderMass) * math.cos(RAMP_ANGLES[1]), 0]).rotate(about_point=(slider.get_critical_point((0,0,0))), angle=-RAMP_ANGLES[1])
         vctD2 = vctD.copy()
 
         # TEXT
-        #forceGravityText = MathTex(r"F_g&", font_size=60).move_to((3,3,0), aligned_edge=UR)
-        # theta = MathTex(r'\theta').move_to(ramp.get_critical_point(DR), aligned_edge=(RIGHT + DOWN)).shift([-RAMP_BASE/8, 0.2, 0])
         theta = MathTex(r'\theta').move_to([RAMP_COORD[1][0] - RAMP_BASE/4 - 0.2 , RAMP_COORD[1][1] + 0.2,0])
         # DIMENSIONS
         rampHeightText = Integer(number=RAMP_HEIGHT).move_to(ramp.get_critical_point(LEFT), aligned_edge=RIGHT).shift([-0.2,0,0])
@@ -66,11 +68,8 @@ class BaseBlock(Scene):
         slider.move_to(SLIDER_REF, aligned_edge=DL)      
 
         # Place the Ramp and the slider on the scene
-        topCorner = Cross(scale_factor=0.1).move_to(ramp.get_critical_point(UL))
-
         self.play(Create(slider),
-                  Create(ramp),
-                  Create(topCorner))
+                  Create(ramp))
         self.wait(1)
 
         # Rotate the slider into the correct angle
@@ -86,9 +85,6 @@ class BaseBlock(Scene):
         self.add(theta)
         self.wait(1)
 
-        """ for d in [(0,0,0), UP, UR, RIGHT, DR, DOWN, DL, LEFT, UL]:
-            self.add(Cross(scale_factor=0.1).move_to(slider.get_critical_point(d))) """
-
         vctX.move_to(slider.get_critical_point((0,0,0)), aligned_edge=(LEFT))
         vctD.move_to(slider.get_critical_point((0,0,0)), aligned_edge=(LEFT + UP))
         vctD2.move_to(slider.get_critical_point((0,0,0)), aligned_edge=(LEFT + UP))
@@ -98,15 +94,16 @@ class BaseBlock(Scene):
 
         # Add Directional vectors
         self.play(Create(vctD),
-                  Create(vctX),
                   Create(vctY),
                   Create(vctN),
                   Create(vctD2))
         self.wait(2)
 
-        # Move D2 to head of N vector
+        # Shift D2 to Tail of N vector
+        nTail = vctN.get_end()
         self.play(
-            vctD2.animate.shift([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1])), 0])
+            #vctD2.animate.shift([-(lengthVector * math.sin(RAMP_ANGLES[1])), -(lengthVector * math.cos(RAMP_ANGLES[1])), 0])
+            vctD2.animate.shift(vctN.get_vector())
         )
         self.wait(2)
 
